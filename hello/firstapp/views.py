680da68b0d920e24059f8061dc39639534747800
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2020-10-12 10:06:05 +0300

    Update views.py

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ArtistForm
 
 
 def index(request):
