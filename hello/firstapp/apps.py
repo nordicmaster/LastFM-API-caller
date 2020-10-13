66cc831e692f93564c9377c236401656ba8db01b
ryabukhinda <ryabukhinda@PCP0138>
2020-10-13 14:06:27 +0300

    fix wrong request data

--- hello/firstapp/apps.py
+++ hello/firstapp/apps.py
@@ -8,20 +8,23 @@ class FirstappConfig(AppConfig):
 
 res = ''
 
+
 def getLastFmInfo(name):
-    if ' ' in name:
-        name = name.replace(" ", "+")
+    #if ' ' in name:
+    #    name = name.replace(" ", "+")
     url = 'https://ws.audioscrobbler.com/2.0/'
     myobj = {'method': 'artist.getinfo',
              'artist': name,
              'api_key': '57ee3318536b23ee81d6b27e36997cde',
              'format': 'json'}
-    x = requests.post(url, data=myobj)
+    x = requests.get(url, myobj)
     xInfo = x.json()
-    print(xInfo)
+    global res
+    if 'error' in xInfo:
+        res = res + xInfo["message"] + "<br>"
+        return res
     listeners = xInfo["artist"]["stats"]["listeners"]
     totalscrobbles = xInfo["artist"]["stats"]["playcount"]
-    global res
     res = res + "<span class=\"fixwidth\">" + name + "</span> has <span class=\"fixwidth2\">"
     res = res + listeners + "</span> listeners and <span class=\"fixwidth3\">"
     res = res + totalscrobbles + "</span> scrobbles. Ratio=" + str(float(totalscrobbles) / float(listeners)) + "<br>";
