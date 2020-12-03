f84b18d57d40c69044027667d5c04a2302fafe70
ryabukhinda <ryabukhinda@PCP0138>
2020-12-03 14:21:14 +0300

    remove js part

--- hello/firstapp/static/script.js
+++ hello/firstapp/static/script.js
@@ -1,17 +1,3 @@
-    async function getInfo(artist) {
-        if (artist.includes(' '))
-            artist = artist.replaceAll(' ', '+');
-        const responseInfo = await fetch("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+artist+"&api_key=57ee3318536b23ee81d6b27e36997cde&format=json");
-        var xresInfo = await responseInfo.json();
-        listeners = xresInfo.artist.stats.listeners;
-        totalscrobbles = xresInfo.artist.stats.playcount;
-        document.getElementById("artists").innerHTML += "<span class=\"fixwidth\">"+
-                                            artist + "</span> has <span class=\"fixwidth2\">" +
-											listeners + "</span> listeners and <span class=\"fixwidth3\">" +
-											totalscrobbles + "</span> scrobbles. Ratio=" +
-											totalscrobbles/listeners + "<br>";
-    }
-
     function clearBase() {
         window.location.href = "deleteAll";
     }
